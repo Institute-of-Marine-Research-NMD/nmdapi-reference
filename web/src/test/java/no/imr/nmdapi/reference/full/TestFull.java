b0c122cc7d545b7f3bdcfcd4907050db1713cd66
KjetilFjellheim <kjetil.fjellheim@imr.no>
2015-09-28 13:24:14 +0200

    Updated tests

--- web/src/test/java/no/imr/nmdapi/reference/full/TestFull.java
+++ web/src/test/java/no/imr/nmdapi/reference/full/TestFull.java
@@ -49,45 +49,60 @@ public class TestFull {
     }
 
     @Test
-    public void testFull() throws Exception {
+    public void testDataQuality() throws Exception {
+        testType("dataquality");
+    }
+
+    @Test
+    public void testPlatform() throws Exception {
+        testType("platform");
+    }
+
+    @Test
+    public void testTaxa() throws Exception {
+        testType("taxa");
+    }
+
+
+    public void testType(String type) throws Exception {
 
         //Get data and verify that nothing is there.
-        mockMvc.perform(get("/dataquality").characterEncoding("UTF-8")).andExpect(status().isNotFound());
+        mockMvc.perform(get("/".concat(type)).characterEncoding("UTF-8")).andExpect(status().isNotFound());
 
         //Insert data.
         mockMvc.perform(
-                post("/dataquality")
+                post("/".concat(type))
                 .contentType(MediaType.APPLICATION_XML)
                 .characterEncoding("UTF-8")
-                .content(FileUtils.readFileToString(new File(this.getClass().getClassLoader().getResource("dataquality_1.xml").getFile()), "UTF-8"))
+                .content(FileUtils.readFileToString(new File(this.getClass().getClassLoader().getResource(type.concat("_1.xml")).getFile()), "UTF-8"))
                 )
                 .andDo(print())
                 .andExpect(status().isOk());
 
         // Verify that data is there.
-        final String insertedFile = FileUtils.readFileToString(new File(this.getClass().getClassLoader().getResource("dataquality_1.xml").getFile()), "UTF-8");
-        mockMvc.perform(get("/dataquality").characterEncoding("UTF-8")).andExpect(status().isOk()).andExpect(content().xml(insertedFile));
+        final String insertedFile = FileUtils.readFileToString(new File(this.getClass().getClassLoader().getResource(type.concat("_1.xml")).getFile()), "UTF-8");
+        mockMvc.perform(get("/".concat(type)).characterEncoding("UTF-8")).andExpect(status().isOk()).andExpect(content().xml(insertedFile));
 
         //Verify list returns as expected
         mockMvc.perform(get("/").characterEncoding("UTF-8")).andExpect(status().isOk());
 
         // Update data.
         mockMvc.perform(
-                put("/dataquality")
+                put("/".concat(type))
                 .contentType(MediaType.APPLICATION_XML)
                 .characterEncoding("UTF-8")
-                .content(FileUtils.readFileToString(new File(this.getClass().getClassLoader().getResource("dataquality_2.xml").getFile()), "UTF-8"))
+                .content(FileUtils.readFileToString(new File(this.getClass().getClassLoader().getResource(type.concat("_2.xml")).getFile()), "UTF-8"))
                 )
                 .andExpect(status().isOk());
 
         // Verify that data is there.
-        String updatedFile = FileUtils.readFileToString(new File(this.getClass().getClassLoader().getResource("dataquality_2.xml").getFile()), "UTF-8");
-        mockMvc.perform(get("/dataquality").characterEncoding("UTF-8")).andExpect(status().isOk()).andExpect(content().xml(updatedFile));
+        String updatedFile = FileUtils.readFileToString(new File(this.getClass().getClassLoader().getResource(type.concat("_2.xml")).getFile()), "UTF-8");
+        mockMvc.perform(get("/".concat(type)).characterEncoding("UTF-8")).andExpect(status().isOk()).andExpect(content().xml(updatedFile));
 
         //Delete data.
-        mockMvc.perform(delete("/dataquality").characterEncoding("UTF-8")).andExpect(status().isOk());
+        mockMvc.perform(delete("/".concat(type)).characterEncoding("UTF-8")).andExpect(status().isOk());
         //Get data and verify that nothing is there.
-        mockMvc.perform(get("/dataquality").characterEncoding("UTF-8")).andExpect(status().isNotFound());
+        mockMvc.perform(get("/".concat(type)).characterEncoding("UTF-8")).andExpect(status().isNotFound());
     }
 
     @Test
